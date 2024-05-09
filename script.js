473faef6b2b12092f1bbae60a6799bbeeec53248
arushigupta3037 <agupta3037@gatech.edu>
2024-05-08 23:07:50 -0400

    added profile picture- still working on size and style with it

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ var typed = new Typed('#typed-text', {
     backSpeed: 20, // Backspacing speed in milliseconds
     loop: false // Whether to loop through the strings
 });
+
+window.addEventListener('load', function() {
+    // Calculate the height of the text
+    var textHeight = document.querySelector('#about .text').offsetHeight;
+
+    // Set the height of the image to match the height of the text
+    document.querySelector('#about-image').style.height = textHeight + 'px';
+});
+
