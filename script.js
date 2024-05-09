654021d5f52140126ae73f8655579770fee90703
arushigupta3037 <agupta3037@gatech.edu>
2024-05-08 22:07:17 -0400

    This has hello world animation and about me. As well as nav menu bar. Colors are wrong

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize Typed.js for the typed text animation
+var typed = new Typed('#typed-text', {
+    strings: ["Arushi Gupta"],
+    typeSpeed: 50, // Typing speed in milliseconds
+    backSpeed: 20, // Backspacing speed in milliseconds
+    loop: true // Whether to loop through the strings
+});
